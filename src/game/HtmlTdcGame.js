fbdbd7163b10193764bead6083b5f260a1f7d8a8
Qoqqi <sazykin.georgii@gmail.com>
2023-01-06 14:08:43 +0300

    Второй вариант HtmlTdcGame

--- src/game/HtmlTdcGame.js
+++ src/game/HtmlTdcGame.js
@@ -5,6 +5,10 @@ export default class HtmlTdcGame {
     constructor() {
         let game = new TdcGame();
 
-        game.loadLevelFromJson(level1);
+        // game.loadLevelFromJson(level1);
+
+        game.createWorld(5, 5);
+        game.setPlayerPosition(1, 1);
+        game.setFinishPosition(5, 5);
     }
 }
