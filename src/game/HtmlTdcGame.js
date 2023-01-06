f286755ffa9cff27778d31c69b542e5acdfab850
Qoqqi <sazykin.georgii@gmail.com>
2023-01-06 14:22:56 +0300

    Третий вариант HtmlTdcGame

--- src/game/HtmlTdcGame.js
+++ src/game/HtmlTdcGame.js
@@ -10,5 +10,38 @@ export default class HtmlTdcGame {
         game.createWorld(5, 5);
         game.setPlayerPosition(1, 1);
         game.setFinishPosition(5, 5);
+
+        this.renderWorld(game.world);
+    }
+
+    renderWorld(world) {
+        this.renderTerrain(world);
+        this.renderObjects(world.getAllObjects());
+    }
+
+    renderTerrain(world) {
+        for (let x = 1; x <= world.width; x++) {
+            for (let y = 1; y <= world.height; y++) {
+                this.renderTerrainTile(x, y);
+            }
+        }
+    }
+
+    renderTerrainTile(x, y) {
+        this.renderTile(x, y, 'grass.png');
+    }
+
+    renderObjects(objects) {
+        for (const object of objects) {
+            this.renderObject(object);
+        }
+    }
+
+    renderObject(object) {
+        this.renderTile(object.x, object.y, 'player.png'); // TODO: Или finish.png и т.д.
+    }
+
+    renderTile(x, y, texture) {
+        // TODO: отображать картинку
     }
 }
