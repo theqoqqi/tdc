bb3befedf03cbd10d1a3c8982d8debdd5c6ab3ce
Qoqqi <sazykin.georgii@gmail.com>
2023-01-07 14:14:41 +0300

    Доработки 1 уровня

--- src/game/levels/level1.js
+++ src/game/levels/level1.js
@@ -2,8 +2,36 @@
 const level1 = {
     width: 5,
     height: 5,
-    start: { x: 1, y: 1 },
-    finish: { x: 5, y: 5 },
+    start: { x: 1, y: 5 },
+    finish: { x: 5, y: 1 },
+    commands: [
+        {
+            type: 'move',
+            steps: [
+                'up',
+            ]
+        },
+        {
+            type: 'move',
+            steps: [
+                'right',
+                'right',
+            ]
+        },
+        {
+            type: 'move',
+            steps: [
+                'up-right',
+            ]
+        },
+        {
+            type: 'move',
+            steps: [
+                'up-right',
+                'up',
+            ]
+        },
+    ]
 };
 
 export default level1;
