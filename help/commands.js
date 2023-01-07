b9decc8636e26b93095048f083eaf9a9dde9aeb0
Qoqqi <sazykin.georgii@gmail.com>
2023-01-07 14:43:55 +0300

    Наброски по использованию команд

---
+++ help/commands.js
@@ -0,0 +1,48 @@
+
+
+let unusedCommands = game.getUnusedCommands(); // Получить список доступных команд на текущем уровне (использованные команды должны пропадать из списка)
+
+for (const command of unusedCommands) {
+    let icon = getIcon(command.type);
+
+    if (command.type === 'move') {
+        let steps = command.steps;
+
+        for (const step of steps) {
+            renderStep(step.direction);
+        }
+    }
+
+    if (command.type === 'attack') {
+        renderAttack(command.damage, command.direction);
+    }
+}
+
+
+
+let usedCommands = game.getUsedCommands(); // Получить список команд, которые уже использованы
+
+for (const command of usedCommands) {
+    let icon = getIcon(command.type);
+
+    if (command.type === 'move') {
+        let steps = command.steps;
+
+        for (const step of steps) {
+            renderStep(step.direction);
+        }
+    }
+
+    if (command.type === 'attack') {
+        renderAttack(command.damage, command.direction);
+    }
+}
+
+
+
+let command = null; // Команда, которую взял мышкой
+
+game.addCommand(command); // Добавляет команду в список использованных и удаляет из доступных
+game.addCommand(command, index); // Добавляет команду в список на определенную позицию использованных и удаляет из доступных
+
+game.removeCommand(command); // Удаляет команду из списка использованных и возвращает в доступные
