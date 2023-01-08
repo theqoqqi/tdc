e03b36fea9485330d659ffd60761766daa5217e2
Qoqqi <sazykin.georgii@gmail.com>
2023-01-08 19:33:50 +0300

    Доработки кнопок команд

---
+++ src/game/gui/CommandElementFactory.js
@@ -0,0 +1,24 @@
+
+export default class CommandElementFactory {
+
+    static #defaultCommandType = (command, $command) => {
+        $command.text(`${command.type} ${command.steps.map(step => step.direction).join(',')}`);
+    };
+
+    static #commandTypes = {
+        move(command, $command) {
+            for (const step of command.steps) {
+                $command.append(`<div class='command-icon step-arrow arrow-${step.direction}'>`);
+            }
+        },
+    };
+
+    static create(command) {
+        let $command = $(`<div class='command'>`);
+        let commandType = this.#commandTypes[command.type] ?? this.#defaultCommandType;
+
+        commandType(command, $command);
+
+        return $command;
+    }
+}
